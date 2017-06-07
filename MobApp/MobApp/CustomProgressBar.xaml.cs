07f324562fc53eb7238ace0ccde5edf392449d9e
Mohamed Rasheed Gomaa (ITLec) <Rasheed@ITLec.com>
2017-06-07 03:10:55 +0300

    Add All Files To Project

---
+++ MobApp/MobApp/CustomProgressBar.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MobApp
+{
+
+    public partial class CustomProgressBar : ContentView
+    {
+        public CustomProgressBar()
+        {
+            InitializeComponent();
+            Init();
+            Reset();
+        }
+
+        private void Init()
+        {
+            if (TaskItems == null)
+            {
+                TaskItems = new List<TaskItem>();
+            }
+        }
+
+        public List<TaskItem> TaskItems
+        {
+            get;
+            set;
+        }
+
+        public async Task Run()
+        {
+            IsRunning = true;
+            double counter = 0;
+
+            double totalCounter = TaskItems.Sum(e => e.Wieght);
+
+
+
+            foreach (var item in TaskItems)
+            {
+                counter = counter + item.Wieght;
+                double currentStep = counter / totalCounter;
+                double currentPercentage = (currentStep * 100);
+                CurrentPercentage = currentPercentage;
+                lbl.Text = string.Format("{0} - (%{1})", item.TaskName, String.Format("{0:0.0}", currentPercentage));
+                item.Task.Start();
+                await mainProgressBar.ProgressTo(currentStep, 0, Easing.Linear);
+            }
+
+            IsRunning = false;
+        }
+
+        public double CurrentPercentage
+        {
+            get;
+            set;
+
+        }
+        public async void Reset()
+        {
+            await mainProgressBar.ProgressTo(0, 0, Easing.Linear);
+            lbl.Text = "%0";
+
+            CurrentPercentage = 0;
+            if (TaskItems != null)
+            {
+                TaskItems.Clear();
+            }
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+    }
+
+
+    public class TaskItem
+    {
+        public Task Task
+        {
+            get;
+            set;
+        }
+        public string TaskName
+        { get; set; }
+
+        public double Wieght
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ MobApp/MobApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            Task<bool> task = Task.Run(()=> DisplayAlert("Simple Alert", "Decide on an option", "yes or ok", "no or cancel"));
+            label.Text = "Alert is currently displayed";
+            bool result = await task;
+            label.Text = String.Format("Alert {0} button was pressed", result ? "OK" : "Cancel");
+
+           await DisplayAlert("Simple Alert", "Decide on an option", "yes or ok", "no or cancel");
+
+            label.Text = "123";
+        }
+
+    async void btnSubmit_Clicked(object sender, EventArgs arg)
+        {
+
+
+            List<TaskItem> tskItems = new List<TaskItem>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                tskItems.Add(new TaskItem { Task = new Task(() => { YourCode(); }), TaskName ="Task#: "+ i.ToString(), Wieght = 100 });
+            }
+            customProgressBar.TaskItems = tskItems;
+           await customProgressBar.Run();
+
+
+        }
+
+        public async void YourCode()
+        {
+            //Add your logic code
+        }
+    }
+    
+}
